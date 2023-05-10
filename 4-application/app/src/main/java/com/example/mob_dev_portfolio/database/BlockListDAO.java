61c2e2c00174d64359383b8b83803ba81c25cab5
c21081270 <LiuF29@cardiff.ac.uk>
2023-05-10 13:40:44 +0100

    block with condition worked

---
+++ 4-application/app/src/main/java/com/example/mob_dev_portfolio/database/BlockListDAO.java
@@ -0,0 +1,23 @@
+package com.example.mob_dev_portfolio.database;
+
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.Query;
+
+import java.util.List;
+
+@Dao
+public interface BlockListDAO {
+
+    @Query("SELECT * FROM blocklist")
+    List<BlockList> getAllBlockList();
+
+    @Query("SELECT * FROM blocklist WHERE phone_no = :phoneNo")
+    boolean getBlockListByPhoneNo(String phoneNo);
+
+    @Insert
+    void insertAll(BlockList... blockLists);
+
+    @Query("DELETE FROM blocklist")
+    void deleteAll();
+}
